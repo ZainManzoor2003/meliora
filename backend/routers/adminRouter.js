e548b9e28afb87b1458de43be9773816eeefcf48
ZainManzoor2003 <101002312+ZainManzoor2003@users.noreply.github.com>
2024-12-24 22:20:29 +0500

    Update adminRouter.js

--- backend/routers/adminRouter.js
+++ backend/routers/adminRouter.js
@@ -4,11 +4,11 @@ const adminController = require('../controllers/adminController');
 // const upload = require('../middlewares/uploadAdminImage');
 
 // router.post('/', upload.single('admin-image'), adminController.createAdmin);
-// router.get('/', adminController.getAllAdmins);
-// router.get('/:id', adminController.getAdminById);
+router.get('/', adminController.getAllAdmins);
+router.get('/:id', adminController.getAdminById);
 // router.put('/:id', upload.single('admin-image'), adminController.updateAdmin);
-// router.put('/role/:id', adminController.updateAdminRole);
-// router.delete('/:id', adminController.deleteAdmin);
+router.put('/role/:id', adminController.updateAdminRole);
+router.delete('/:id', adminController.deleteAdmin);
 
 
 module.exports = router;
