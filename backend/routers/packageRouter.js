0d3a0f809790e5db0ca94f38af0d094c38136bd2
ZainManzoor2003 <101002312+ZainManzoor2003@users.noreply.github.com>
2024-12-24 22:15:45 +0500

    Update packageRouter.js

--- backend/routers/packageRouter.js
+++ backend/routers/packageRouter.js
@@ -3,10 +3,10 @@ const router = express.Router();
 const packageController = require('../controllers/packageController');
 // const upload = require('../middlewares/uploadPackageImage');
 
-router.post('/', upload.single('image'), packageController.createPackage);
-router.get('/', packageController.getAllPackages);
-router.get('/:id', packageController.getPackageById);
-router.put('/:id', upload.single('image'), packageController.updatePackage);
-router.delete('/:id', packageController.deletePackage);
+// router.post('/', upload.single('image'), packageController.createPackage);
+// router.get('/', packageController.getAllPackages);
+// router.get('/:id', packageController.getPackageById);
+// router.put('/:id', upload.single('image'), packageController.updatePackage);
+// router.delete('/:id', packageController.deletePackage);
 
 module.exports = router;
