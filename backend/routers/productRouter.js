ec903da2ed8c79582ebb6a657b9890940f9fba6e
ZainManzoor2003 <101002312+ZainManzoor2003@users.noreply.github.com>
2024-12-24 22:21:18 +0500

    Update productRouter.js

--- backend/routers/productRouter.js
+++ backend/routers/productRouter.js
@@ -1,12 +1,12 @@
 const express = require('express');
 const router = express.Router();
 const productController = require('../controllers/productController');
-const { uploadMultiple, multerErrorHandler } = require('../middlewares/uploadProductImages');
+// const { uploadMultiple, multerErrorHandler } = require('../middlewares/uploadProductImages');
 
-router.post('/', uploadMultiple, productController.createProduct, multerErrorHandler);
+// router.post('/', uploadMultiple, productController.createProduct, multerErrorHandler);
 router.get('/', productController.getAllProducts);
 router.get('/:id', productController.getProductById);
-router.put('/:id',uploadMultiple, productController.updateProduct,multerErrorHandler);
+// router.put('/:id',uploadMultiple, productController.updateProduct,multerErrorHandler);
 router.delete('/:id', productController.deleteProduct);
 router.get('/usage/:id', productController.getProductUsage);
 router.put('/usage/:id', productController.updateProductUsage);
